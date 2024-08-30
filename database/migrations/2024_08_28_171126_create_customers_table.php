8c398507f3fd8f59b842edab192286cb9f473601
Khalid Taher <k.taher@bootfi.com>
2024-08-31 02:30:26 +0300

    add fillement

--- database/migrations/2024_08_28_171126_create_customers_table.php
+++ database/migrations/2024_08_28_171126_create_customers_table.php
@@ -13,11 +13,22 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->unique()->nullable();
+            $table->string('gender')->default('male');
             $table->string('email')->unique();
-            $table->string('phone');
-            $table->text('address');
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('birth_date')->nullable();
+            $table->string('avatar')->nullable();
+            $table->boolean('active')->default(true);
+           // $table->foreignUuid('country_id')->nullable()->references('id')->on('countries');
+            $table->softDeletes();
+            $table->rememberToken();
             $table->timestamps();
+
+           // $table->index(['last_name', 'first_name', 'email', 'active', 'gender']);
         });
     }
 
