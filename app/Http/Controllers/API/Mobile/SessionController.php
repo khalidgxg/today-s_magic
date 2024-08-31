48b63c7b3eb8361ef68253021f4cf6bf0ad0ca94
Khalid Taher <k.taher@bootfi.com>
2024-08-31 23:15:27 +0300

    updates

---
+++ app/Http/Controllers/API/Mobile/SessionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Mobile;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+}
--- routes/mobile.php
+++ routes/mobile.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware(['auth:sanctum', 'active'])->group(function () {
+
+});
